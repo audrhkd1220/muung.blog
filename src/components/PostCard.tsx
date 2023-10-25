d462ea936899a361b1f8763c1f3987a8f97248e1
audrhkd1220 <audrhkd63@naver.com>
2023-10-25 23:18:39 +0900

    style: Edit PostCard

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -3,19 +3,29 @@ import Link from 'next/link';
 import { krDateFormatter } from '@/lib/date';
 
 const PostCard = (post: Post) => {
-  console.log(post);
   return (
     <div className="mb-8">
-      <Link
-        href={post.url}
-        className="text-black hover:text-blue-900 dark:text-blue-400"
-      >
-        <h2 className="mb-1 text-2xl text-bold">{post.title}</h2>
+      <Link href={post.url} className="group">
+        <h2 className="group-hover:underline mb-1 text-2xl text-bold">
+          {post.title}
+        </h2>
+        <h3 className="text-xl mb-2">{post.description}</h3>
       </Link>
-      <h3 className="text-xl">{post.description}</h3>
-      <time dateTime={post.date} className="mb-2 block text-xs text-gray-600">
-        {krDateFormatter(post.date)}
-      </time>
+      <div className="flex">
+        <div className="flex grow gap-4">
+          {post.tags.map((tag) => (
+            <div
+              className="px-2 py-0.5 bg-[#d4f0f0] rounded-2xl text-gray-900 text-sm hover:bg-[#8FCACA]"
+              key={tag}
+            >
+              <Link href={post.url}>{tag}</Link>
+            </div>
+          ))}
+        </div>
+        <time dateTime={post.date} className="mb-2 block text-xs text-gray-600">
+          {krDateFormatter(post.date)}
+        </time>
+      </div>
     </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { allPosts } from '@/lib/post';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import PostCard from '@/components/PostCard';
+import Link from 'next/link';
 
 export default function Home() {
   const posts = allPosts.sort((a, b) =>
@@ -14,10 +15,16 @@ export default function Home() {
       <Header />
       <main className="w-full pt-[70px] mx-auto max-w-3xl px-6 lg:max-w-6xl lg:px-5">
         <section className="pt-6">
-          <p>Muung blog에 오신 것을 환영합니다.🥳</p>
-          <p>
+          <h2 className="text-3xl font-bold mb-2">Welcome to muung.blog 🥳</h2>
+          <p className="mb-2">
             이곳은 개발하면서 경험한 것을 기억하기 위해 기록하는 공간입니다.
           </p>
+          <div className="flex gap-3">
+            <p>정우석에 대해 더 알고싶다면</p>
+            <Link href="/about" className="hover:underline">
+              about me
+            </Link>
+          </div>
         </section>
         <section className="pt-6">
           <div className="my-6">
